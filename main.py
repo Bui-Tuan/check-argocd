c84a6d6a29a129375fd3777be9a57334098488ab
Bui-Tuan <71039674+Bui-Tuan@users.noreply.github.com>
2025-03-15 03:22:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World and hello tuan!'
+    return 'Hello, World and hello tuan! \n it all right!'
 
 
 @app.route('/health')
